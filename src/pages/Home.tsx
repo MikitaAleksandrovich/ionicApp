58b4e73a776efa2b53f96f0ed97c9bedff1864f0
MikitaAleksandrovich <aleksandrovich.mikita@gmail.com>
2019-11-13 22:58:39 +0300

    Add some simple functionality using ionic components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,61 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, 
+        IonHeader, 
+        IonPage, 
+        IonTitle, 
+        IonToolbar, 
+        IonSearchbar, 
+        IonList, 
+        IonBadge, 
+        IonLabel, 
+        IonNote, 
+        IonCheckbox, 
+        IonItem, 
+        IonFab, 
+        IonFabButton, 
+        IonIcon } from '@ionic/react';
 import React from 'react';
+import { RouteComponentProps } from 'react-router';
 
-const Home: React.FC = () => {
+import { add } from 'ionicons/icons';
+
+const Home: React.FC<RouteComponentProps> = (props) => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonTitle>Ionic Blank</IonTitle>
         </IonToolbar>
+        <IonSearchbar></IonSearchbar>
       </IonHeader>
-      <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
-      </IonContent>
+      <IonContent>
+    <IonList>
+      <IonItem>
+        <IonCheckbox slot="start" />
+        <IonLabel>
+          <h1>Create Idea</h1>
+          <IonNote>Run Idea by Brandy</IonNote>
+        </IonLabel>
+        <IonBadge color="success" slot="end">
+          5 Days
+        </IonBadge>
+      </IonItem>
+      <IonItem>
+        <IonCheckbox slot="start"/>
+        <IonLabel>
+          <h1>Set up goals</h1>
+          <IonNote>For every week</IonNote>
+        </IonLabel>
+        <IonBadge color="success" slot="end">
+          1 Day
+        </IonBadge>
+      </IonItem>
+    </IonList>
+    <IonFab vertical="bottom" horizontal="end" slot="fixed">
+      <IonFabButton  onClick={() => props.history.push('./new')}>
+        <IonIcon icon={add} />
+      </IonFabButton>
+    </IonFab>
+  </IonContent>
     </IonPage>
   );
 };
